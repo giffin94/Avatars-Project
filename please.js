59cf8eb05744230fe29695df584ebe746b11e46d
Aedan <aedan.giffin@gmail.com>
2019-01-16 04:08:39 +0000

    Practiced handling different errors - committing minor changes, no new functionality

--- please.js
+++ please.js
@@ -1,7 +1,6 @@
-require("dotenv").load();
 var request = require('request');
 var fs = require('fs');
-const imageFolder = './avatars/';
+//const imageFolder = './avatars/';
 
 var please = {
     getThisStuff: function (options, cb) {
@@ -9,7 +8,7 @@ var please = {
             cb(err, res, body);
         }) //goes to the requested path on Github and invokes a specified callback - VERY GENERAL USE
     },
-    showDownloadResponse: function (response) {
+    showDownloadResponse: function (response, onSuccess) {
         console.log('Response Status Code: ', response.statusMessage, response.headers['content-type']);
         console.log('Download starting...'); //Console log on successful initiation of download
     },
@@ -19,23 +18,23 @@ var please = {
     handleNoFolder: function (err) {
         if(err.code === 'ENOENT'){
             console.log("No 'avatars' directory! Please create an empty avatars folder");
+            process.exit();
         } else {
             console.log(err); //Lets the user know when there is a missing folder
         }
     },
-    checkForErrorsAndGo: function (errorCode, userInfo, cb) {
+    checkForErrorsAndGo: function (errorCode, onSuccess) {
         if (errorCode === 404) {
             console.log(`Error! The repo you are looking for doesn't exist! Check your spelling. Error: `, errorCode);
-        } else if (errorCode > 500) {
+            process.exit();
+        } else if (errorCode >= 500) {
             console.log('Uh-oh! Looks like Github is having some troubles... Error: ', errorCode);
+            process.exit();
         } else if (errorCode === 401) {
             console.log('Your personal access token is invalid, try updating it. Error: ', errorCode);
+            process.exit();
         } else {
-            userInfo.forEach(function(element, i) {
-            var uName = userInfo[i]["login"];
-            var thisFilePath = `${imageFolder}${uName}.jpg`;
-            cb(userInfo[0]["avatar_url"], thisFilePath);
-        });
+            onSuccess();
         } //UNDER CONSTRUCTION - I want this to check for error codes from gitHub and if all is well, perform a specified *MODULAR* task. I think I need another callback...
     },
 };
